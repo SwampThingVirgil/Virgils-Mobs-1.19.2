a52f975a0350f3d9483386f75c658ffc6520a596
Virgil <serenemrys1379@gmail.com>
2023-02-09 19:20:58 -0800

    test

---
+++ src/main/java/net/swampthingvirgil/virgilsmobs/VirgilsMobs.java
@@ -0,0 +1,17 @@
+package net.swampthingvirgil.virgilsmobs;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//test stuff
+
+public class VirgilsMobs implements ModInitializer {
+	public static final String MOD_ID = "virgilsmobs";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/swampthingvirgil/virgilsmobs/VirgilsMobsClient.java
@@ -0,0 +1,10 @@
+package net.swampthingvirgil.virgilsmobs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VirgilsMobsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
